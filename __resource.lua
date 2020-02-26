dfafbf4086d0b9e2d717249a8c40daff44293b36
Lukáš Müller <61428713+V1perCZ@users.noreply.github.com>
2020-02-26 07:19:00 +0100

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+-- ©Apollo https://discord.gg/7Km9ATw
+files{
+    "index.html",
+    "app.js",
+    "image1.jpg",
+    "particles.js",
+    "style.css",
+    "music/music.ogg",
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
